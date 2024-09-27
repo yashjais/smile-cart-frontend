32677a6ff73ac4f4965163121a436413e2e2c52c
yashjais <jais.yash01@gmail.com>
2024-09-27 14:51:54 +0530

    Added submission handler to forms

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,6 +1,26 @@
+import {
+  keysToCamelCase,
+  serializeKeysToSnakeCase,
+} from "@bigbinary/neeto-cist";
 import axios from "axios";
-import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
+import { t } from "i18next";
+import { Toastr } from "neetoui";
 import { evolve } from "ramda";
+
+const shouldShowToastr = response =>
+  typeof response === "object" && response?.noticeCode;
+
+const showSuccessToastr = response => {
+  if (shouldShowToastr(response.data)) Toastr.success(response.data);
+};
+
+const showErrorToastr = error => {
+  if (error.message === t("error.networkError")) {
+    Toastr.error(t("error.noInternetConnection"));
+  } else if (error.response?.status !== 404) {
+    Toastr.error(error);
+  }
+};
 
 const requestInterceptors = () => {
   axios.interceptors.request.use(
@@ -20,11 +40,19 @@ const transformResponseKeysToCamelCase = response => {
 };
 
 const responseInterceptors = () => {
-  axios.interceptors.response.use(response => {
-    transformResponseKeysToCamelCase(response);
+  axios.interceptors.response.use(
+    response => {
+      transformResponseKeysToCamelCase(response);
+      showSuccessToastr(response);
 
-    return response.data;
-  });
+      return response.data;
+    },
+    error => {
+      showErrorToastr(error);
+
+      return Promise.reject(error);
+    }
+  );
 };
 
 export default function initializeAxios() {
